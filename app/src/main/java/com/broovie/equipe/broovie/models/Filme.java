d042d425ef386127b7b0e7af47df0da21de13ba2
Marcelo Hugo <marcellohugo01@gmail.com>
2018-10-21 14:04:10 -0300

    Criação dos principais models com métodos basicos

---
+++ app/src/main/java/com/broovie/equipe/broovie/models/Filme.java
@@ -0,0 +1,40 @@
+package com.broovie.equipe.broovie.models;
+
+public class Filme {
+    private String nome;
+    private Genero genero;
+    private String sinopse;
+    private int classificacaoIndicativa;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Genero getGenero() {
+        return genero;
+    }
+
+    public void setGenero(Genero genero) {
+        this.genero = genero;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+    public int getClassificacaoIndicativa() {
+        return classificacaoIndicativa;
+    }
+
+    public void setClassificacaoIndicativa(int classificacaoIndicativa) {
+        this.classificacaoIndicativa = classificacaoIndicativa;
+    }
+}
---
+++ app/src/main/java/com/broovie/equipe/broovie/models/Genero.java
@@ -0,0 +1,13 @@
+package com.broovie.equipe.broovie.models;
+
+public class Genero {
+    private String descricao;
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+}
