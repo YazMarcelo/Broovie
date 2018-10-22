c296f7368e706a56ba862e58cc13bfbd7293997f
Marcelo Hugo <marcellohugo01@gmail.com>
2018-10-22 00:53:35 -0300

    Iniciando tela de login e tela de Cadastro de usuário

---
+++ app/src/main/java/com/broovie/equipe/broovie/activities/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.broovie.equipe.broovie.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.broovie.equipe.broovie.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
