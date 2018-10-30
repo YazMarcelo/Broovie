1b49d7e38bcf8adf2cd5f43408cf4292664d5225
Marcelo Hugo <marcellohugo01@gmail.com>
2018-10-29 21:38:22 -0300

    Atividade tela principal iniciada.

---
+++ app/src/main/java/com/broovie/equipe/broovie/activities/TelaPrincipalActivity.java
@@ -0,0 +1,15 @@
+package com.broovie.equipe.broovie.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.broovie.equipe.broovie.R;
+
+public class TelaPrincipalActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_principal);
+    }
+}
