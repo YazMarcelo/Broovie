662c8d20a456db84ec3035591bc1d93521b28e86
Marcelo Hugo <marcellohugo01@gmail.com>
2018-11-13 22:00:19 -0200

    Chamando tela Principal da tela de login

--- app/src/main/java/com/broovie/equipe/broovie/activities/LoginActivity.java
+++ app/src/main/java/com/broovie/equipe/broovie/activities/LoginActivity.java
@@ -7,6 +7,7 @@ import android.view.View;
 
 import com.broovie.equipe.broovie.R;
 import com.broovie.equipe.broovie.Util.DebugActivity;
+import com.broovie.equipe.broovie.activities.TelaPrincipal.TelaPrincipalActivity;
 import com.broovie.equipe.broovie.activities.Usuario.CadastroUsuarioActivity;
 
 public class LoginActivity extends DebugActivity {
@@ -31,12 +32,11 @@ public class LoginActivity extends DebugActivity {
                 break;
             case R.id.btn_login:
                 //chamar tela
-//                Log.i(TAG, "\t\tCall view Category");
-//
-//                it = new Intent(this, CategoryActivity.class);
-//                params = new Bundle();
-//                params.putString("nome","Tela de Categoria");
-//                startActivity(it);
+                Log.i(TAG, "\t\tTela Principal");
+                it = new Intent(LoginActivity.this, TelaPrincipalActivity.class);
+                params = new Bundle();
+                params.putString("nome","Tela Principal");
+                startActivity(it);
                 break;
             default:
                 Log.i(TAG, String.format("id: %s", view.getId()));
