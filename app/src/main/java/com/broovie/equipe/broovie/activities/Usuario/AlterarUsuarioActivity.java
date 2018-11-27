b8cf3d72cae215cd722223ac7a71b552bbed63d9
Marcelo Hugo <marcellohugo01@gmail.com>
2018-11-27 20:58:50 -0200

    Correção de BUG

---
+++ app/src/main/java/com/broovie/equipe/broovie/activities/Usuario/AlterarUsuarioActivity.java
@@ -0,0 +1,15 @@
+package com.broovie.equipe.broovie.activities.Usuario;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.broovie.equipe.broovie.R;
+
+public class AlterarUsuarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alterar_usuario);
+    }
+}
